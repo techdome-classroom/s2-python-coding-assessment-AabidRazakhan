3549dc0e8730df2b5a42708b79ef5cb71ffd12a7
Aabid Raza Khan <ak3521553@gamil.com>
2024-09-27 14:10:04 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,7 +6,8 @@ class Solution(object):
         """
         stack =[]
         matching_parentheses = {')':'(','}':'{',']':'['}
-        
+        for char in s:
+            if char in
         pass
 
 
