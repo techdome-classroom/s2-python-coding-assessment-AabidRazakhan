3aa473a6523093c963c1f20648422468b78c36ca
Aabid Raza Khan <ak3521553@gamil.com>
2024-09-27 14:10:35 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -7,7 +7,7 @@ class Solution(object):
         stack =[]
         matching_parentheses = {')':'(','}':'{',']':'['}
         for char in s:
-            if char in matching_parentheses:top
+            if char in matching_parentheses:top_element
         pass
 
 
