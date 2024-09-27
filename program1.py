3dc100b37e6b98f4d6079f33a44d147c96e11790
Aabid Raza Khan <ak3521553@gamil.com>
2024-09-27 14:17:45 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -10,6 +10,7 @@ class Solution(object):
             if char in matching_parentheses:
                 top_element = stack.pop() if stack else '#'
                 if matching_parentheses[char]!=top_element:
+                    return False
             
         else:
             stack.append(char)
